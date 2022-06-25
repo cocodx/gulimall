a1eae3bc5e3392d46891b3f20f95db77603cf301
刘刚 <2cevening@gmail.com>
2022-06-26 01:56:30 +0800

    feature(gulimall-gateway):网关服务

---
+++ gulimall-gateway/src/main/java/com/lg/gulimall/gateway/GulimallGatewayApplication.java
@@ -0,0 +1,19 @@
+package com.lg.gulimall.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author amazfit
+ * @date 2022-06-26 上午1:44
+ **/
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@EnableDiscoveryClient
+public class GulimallGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GulimallGatewayApplication.class,args);
+    }
+}
