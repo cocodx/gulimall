b93fbd508417dae2e6c2e6aa8ba33e4d01338fd1
刘刚 <2cevening@gmail.com>
2022-07-02 23:23:03 +0800

    feature(gulimall-gateway,gulimall-minio):gateway配置交给nacos管理，minio启动类添加服务发现注解

--- gulimall-minio/src/main/java/com/lg/gulimall/minio/GulimallMinioStarter.java
+++ gulimall-minio/src/main/java/com/lg/gulimall/minio/GulimallMinioStarter.java
@@ -2,12 +2,14 @@ package com.lg.gulimall.minio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author amazfit
  * @date 2022-07-02 下午5:06
  **/
 @SpringBootApplication
+@EnableDiscoveryClient
 public class   GulimallMinioStarter {
 
     public static void main(String[] args) {
