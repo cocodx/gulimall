9350ed365144f11267191170253d20e92d9e6ae5
刘刚 <2cevening@gmail.com>
2022-07-21 02:54:50 +0800

    feature(gulimall-gateway,gulimall-ware):整合ware服务 & 获取仓库列表

--- gulimall-ware/src/main/java/com/lg/gulimall/ware/GulimallWareApplication.java
+++ gulimall-ware/src/main/java/com/lg/gulimall/ware/GulimallWareApplication.java
@@ -4,18 +4,20 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 /**
  * @author amazfit
  * @date 2022-06-23 下午11:22
  **/
+@EnableTransactionManagement
 @SpringBootApplication
 @MapperScan("com.lg.gulimall.ware.dao")
 @EnableDiscoveryClient
 public class GulimallWareApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(GulimallWareApplication.class,args);
+        SpringApplication.run(GulimallWareApplication.class, args);
     }
 }
 
--- gulimall-ware/src/main/java/com/lg/gulimall/ware/service/impl/WareInfoServiceImpl.java
+++ gulimall-ware/src/main/java/com/lg/gulimall/ware/service/impl/WareInfoServiceImpl.java
@@ -1,16 +1,17 @@
 package com.lg.gulimall.ware.service.impl;
 
-import org.springframework.stereotype.Service;
-import java.util.Map;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.lg.gulimall.common.utils.PageUtils;
 import com.lg.gulimall.common.utils.Query;
-
 import com.lg.gulimall.ware.dao.WareInfoDao;
 import com.lg.gulimall.ware.entity.WareInfoEntity;
 import com.lg.gulimall.ware.service.WareInfoService;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+
+import java.util.Map;
 
 
 @Service("wareInfoService")
@@ -18,9 +19,18 @@ public class WareInfoServiceImpl extends ServiceImpl<WareInfoDao, WareInfoEntity
 
     @Override
     public PageUtils queryPage(Map<String, Object> params) {
+        QueryWrapper<WareInfoEntity> wrapper = new QueryWrapper<>();
+
+        String key = (String) params.get("key");
+        if (!StringUtils.isEmpty(key)) {
+            wrapper.eq("id", key).or().like("name", key)
+                    .or().like("address", key)
+                    .or().like("areacode", key);
+        }
+
         IPage<WareInfoEntity> page = this.page(
                 new Query<WareInfoEntity>().getPage(params),
-                new QueryWrapper<WareInfoEntity>()
+                wrapper
         );
 
         return new PageUtils(page);
