57d961f7986cca925bd7efc17fa5466f80235e0d
刘刚 <2cevening@gmail.com>
2022-07-21 01:52:24 +0800

    bugfix(gulimall-product):SpuInfoDescEntity的id，mybatisplus指定为输入

--- gulimall-product/src/main/java/com/lg/gulimall/product/entity/SpuInfoDescEntity.java
+++ gulimall-product/src/main/java/com/lg/gulimall/product/entity/SpuInfoDescEntity.java
@@ -1,15 +1,15 @@
 package com.lg.gulimall.product.entity;
 
+import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
 
 import java.io.Serializable;
-import java.util.Date;
-import lombok.Data;
 
 /**
  * spu信息介绍
- * 
+ *
  * @author liugang
  * @email 2cevening@gmail.com
  * @date 2022-06-23 22:08:59
@@ -17,16 +17,16 @@ import lombok.Data;
 @Data
 @TableName("pms_spu_info_desc")
 public class SpuInfoDescEntity implements Serializable {
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	/**
-	 * 商品id
-	 */
-	@TableId
-	private Long spuId;
-	/**
-	 * 商品介绍
-	 */
-	private String decript;
+    /**
+     * 商品id
+     */
+    @TableId(type = IdType.INPUT)
+    private Long spuId;
+    /**
+     * 商品介绍
+     */
+    private String decript;
 
 }
